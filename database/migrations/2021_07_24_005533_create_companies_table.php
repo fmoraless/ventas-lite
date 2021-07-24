409b3cb125941395643911c662e7bfd4f9db6b75
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-07-24 00:25:59 -0400

    Create 2021_07_24_005533_create_companies_table.php

---
+++ database/migrations/2021_07_24_005533_create_companies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',255);
+            $table->text('address',500)->nullable();
+            $table->string('phone',10)->nullable();
+            $table->string('taxpayer_id',20)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
