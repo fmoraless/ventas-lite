83a356eff1cffe870d09b2fbd2416d814099c383
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-07-25 00:28:28 -0400

    Sale model and migration

---
+++ database/migrations/2021_07_24_055253_create_sales_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('total', 10,2);
+            $table->integer('items');
+            $table->decimal('cash', 10,2);
+            $table->decimal('change', 10,2);
+            $table->enum('status', ['PAID', 'PENDING','CANCELLED'])->default('PAID');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
