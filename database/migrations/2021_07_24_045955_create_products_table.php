7ef6f6f1fb061f4ebade2affeb0297b58dba5afc
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-07-25 00:28:09 -0400

    Product model and migration

---
+++ database/migrations/2021_07_24_045955_create_products_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('barcode', 25)->nullable();
+            $table->decimal('cost', 10,2)->default(0);
+            $table->decimal('price', 10,2)->default(0);
+            $table->integer('stock');
+            $table->integer('alerts');
+            $table->string('image', 100)->nullable();
+            $table->unsignedBigInteger('category_id');
+            $table->foreign('category_id')->references('id')->on('categories');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
