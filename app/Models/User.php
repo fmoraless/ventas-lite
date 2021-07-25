aae637205c202a7b8987a3a21ac00cc2db678a21
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-07-25 00:31:00 -0400

    additional fields on User model and migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -20,6 +20,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'profile',
+        'phone',
+        'status',
+        'image'
     ];
 
     /**
