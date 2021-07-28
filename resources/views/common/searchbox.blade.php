d6bfcecd0450a8b1bf3141d66db09300dc19bc9f
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-07-27 23:34:28 -0400

    Create searchbox.blade.php

---
+++ resources/views/common/searchbox.blade.php
@@ -0,0 +1,12 @@
+<div class="row justify-content-between">
+    <div class="col-lg-4 col-md-4 col-sm-12">
+        <div class="input-group mb-4">
+            <div class="input-group-prepend">
+                <span class="input-group-text input-gp">
+                    <i class="fas fa-search"></i>
+                </span>
+            </div>
+            <input type="text" wire:model="search" placeholder="Buscar" class="form-control">
+        </div>
+    </div>
+</div>
