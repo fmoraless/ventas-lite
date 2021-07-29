9185d97cfd33b40451fce056a382bbe257693eef
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-07-29 00:17:31 -0400

    modal common views

--- resources/views/common/modalFooter.blade.php
+++ resources/views/common/modalFooter.blade.php
@@ -3,7 +3,7 @@
         <button type="button" wire:click.prevent="resetUI()" class="btn btn-dark close-btn text-info" data-dismiss="modal">
             CERRAR
         </button>
-        @if(selected_id < 1)
+        @if($selected_id < 1)
             <button type="button" wire:click.prevent="Store()" class="btn btn-dark close-modal">
                 GUARDAR
             </button>
--- resources/views/common/modalHead.blade.php
+++ resources/views/common/modalHead.blade.php
@@ -1,7 +1,7 @@
-<div wire:ignore.self class="modal fade" tabindex="-1" role="dialog">
+<div wire:ignore.self class="modal fade" id="theModal" tabindex="-1" role="dialog">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
-            <div class="modal-header">
+            <div class="modal-header bg-dark">
                 <h5 class="modal-title text-white">
                     <b>{{$componentName}}</b> | {{ $selected_id > 0 ? 'Editar' : 'Crear' }}
                 </h5>
