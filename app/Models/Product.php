39990aa9c49180bbe95cbefd4e96ffc0f417b91f
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-02 01:56:10 -0400

    Image Accesor

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -23,4 +23,12 @@ class Product extends Model
     {
         return $this->belongsTo(Category::class);
     }
+
+    public function getImagenAttribute()
+    {
+        if (file_exists('storage/products/' . $this->image))
+            return $this->image;
+        else
+            return 'no-image.png';
+    }
 }
