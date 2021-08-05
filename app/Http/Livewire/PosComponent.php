5af42ab43431f91715585c7d3ca247fe7052ebaa
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-04 20:48:32 -0400

    Post Component variables

--- app/Http/Livewire/PosComponent.php
+++ app/Http/Livewire/PosComponent.php
@@ -2,12 +2,16 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Denomination;
 use Livewire\Component;
 
 class PosComponent extends Component
 {
+    public $total, $cart =[], $itemsQuantity=5, $denominations=[], $efectivo, $change;
+
     public function render()
     {
+        $this->denominations = Denomination::all();
         return view('livewire.pos.component')
             ->extends('layouts.theme.app')
             ->section('content');
