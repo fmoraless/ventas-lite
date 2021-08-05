e82a0593a964981bd8d6100c3096b33298acd39b
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-04 20:48:58 -0400

    sales view compoenent

--- resources/views/livewire/pos/component.blade.php
+++ resources/views/livewire/pos/component.blade.php
@@ -1,3 +1,21 @@
 <div>
-    COMPONENTE DE VENTAS
+    <style></style>
+
+    <div class="row layout-top-spacing">
+        <div class="col-sm-12 col-md-8">
+            <!-- DETALLES -->
+            @include('livewire.pos.partials.detail')
+        </div>
+
+        <div class="col-sm-12 col-md-4">
+            <!-- TOTAL -->
+        @include('livewire.pos.partials.total')
+
+        <!-- MONEDAS -->
+            @include('livewire.pos.partials.coins')
+        </div>
+    </div>
 </div>
+
+<script>
+</script>
