3d7c97308486b97fa95d2212c3086488421364d1
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-10 18:30:19 -0400

    Update component.blade.php

--- resources/views/livewire/pos/component.blade.php
+++ resources/views/livewire/pos/component.blade.php
@@ -1,21 +1,29 @@
 <div>
     <style></style>
-
     <div class="row layout-top-spacing">
         <div class="col-sm-12 col-md-8">
-            <!-- DETALLES -->
+            <!--DETALLES-->
             @include('livewire.pos.partials.detail')
+
+
         </div>
 
         <div class="col-sm-12 col-md-4">
-            <!-- TOTAL -->
+            <!--TOTAL-->
         @include('livewire.pos.partials.total')
 
-        <!-- MONEDAS -->
+        <!--DENOMIACIONES-->
             @include('livewire.pos.partials.coins')
         </div>
     </div>
 </div>
 
-<script>
-</script>
+<script src="{{ asset('js/keypress.js')}}"></script>
+<script src="{{ asset('js/onscan.js')}}"></script>
+
+<script src="{{ asset('js/keypress.js') }}"></script>
+<script src="{{ asset('js/onscan.js') }}"></script>
+@include('livewire.pos.scripts.shortcuts')
+@include('livewire.pos.scripts.events')
+@include('livewire.pos.scripts.general')
+@include('livewire.pos.scripts.scan')
