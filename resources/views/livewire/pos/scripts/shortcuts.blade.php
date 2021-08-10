01224103a4fb0c3c6e88634812d16f9d35af387d
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-10 18:36:30 -0400

    Update shortcuts.blade.php

--- resources/views/livewire/pos/scripts/shortcuts.blade.php
+++ resources/views/livewire/pos/scripts/shortcuts.blade.php
@@ -1,12 +1,14 @@
 <script>
     var listener = new window.keypress.Listener();
 
-    listener.simple_combo("f9", function () {
+    listener.simple_combo("f6", function () {
        livewire.emit('saveSale')
     });
     listener.simple_combo("f8", function () {
         document.getElementById('cash').value = ''
+        document.getElementById('hiddenTotal').value =''
         document.getElementById('cash').focus()
+
     });
     listener.simple_combo("f4", function () {
         var total = parseFloat(document.getElementById('hiddenTotal').value)
