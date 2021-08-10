98f96fc4b693e47fb5d3890a9e2d42454be2b46d
Francisco Morales <fcomorales.sanchez@gmail.com>
2021-08-10 18:36:18 -0400

    Update general.blade.php

--- resources/views/livewire/pos/scripts/general.blade.php
+++ resources/views/livewire/pos/scripts/general.blade.php
@@ -1,14 +1,21 @@
 <script>
-    $('.tblscroll').nicescroll({
-        cursorcolor: "#515365",
-        cursorwidth: "30px",
-        background: "rgba(20,20,20,0.3)",
-        cursorborder: "0px",
-        cursorborderradius: 3
-    })
+    document.addEventListener('DOMContentLoaded', function() {
+		$('.tblscroll').niceScroll({
+			cursoscolor: "#515365",
+			cursorwidth: "30px",
+			background: "rgba(20,20,20,0.3)",
+			cursorborder: "0px",
+			cursorborderradius:3
+
+		})
+
+		
+
+	})
 
     function Confirm(id, eventName, text)
     {
+
         swal({
             title: 'CONFIRMAR',
             text: text,
@@ -17,12 +24,12 @@
             cancelButtonText: 'Cerrar',
             cancelButtonColor: '#fff',
             confirmButtonText: 'Aceptar',
-            confirmButtonColor: '#3B3F5C'
-        }).then(function (result) {
-            if (result.value){
+            confirmButtonColor: '#3b3f5c',
+        }).then(function(result){
+            if(result.value){
                 window.livewire.emit(eventName, id)
                 swal.close()
             }
-        });
+        })
     }
 </script>
